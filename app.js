492e82fedeeb34d00ecc116e39354c277e5749cf
hesham1010 <hesham.gublootech@gmail.com>
2024-05-29 01:44:18 +0500

    intial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const shopRouter = require("./routes/shop");
+const adminRouter = require("./routes/admin");
+const path = require("path");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/admin", adminRouter);
+app.use("/", shopRouter);
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+app.listen(3000);
---
+++ routes/shop.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+const rootDir = require("../util/path");
+router.get("/", (req, res) => {
+  console.log(rootDir);
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
+});
+module.exports = router;
